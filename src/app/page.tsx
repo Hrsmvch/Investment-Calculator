ac0e1f8e8978dcac4b57cd38976b8c5e2f7cda19
Halyna Harasymovych <halyna.harasymovych@tallium.com>
2025-04-03 13:33:05 +0300

    Update investment form styles. Add validation.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,19 +9,15 @@ export default function Home() {
   const [initialInvestment, setInitialInvestment] = useState(0);
 
   return (
-    <>
-      <header>
-        <h1>Investment Calculator</h1>
-        <p>
-          Calculate your future investment based on current rates, compounding
-          frequency, and investment amount.
-        </p>
-      </header>
-
-      <main>
-        <InvestmentForm handleSetResult={setResults} handleSetInitialInvestment={setInitialInvestment} />
-        <InvestmentResultTable results={results} initialInvestment={initialInvestment} />
-      </main>
-    </>
+    <main>
+      <InvestmentForm
+        handleSetResult={setResults}
+        handleSetInitialInvestment={setInitialInvestment}
+      />
+      <InvestmentResultTable
+        results={results}
+        initialInvestment={initialInvestment}
+      />
+    </main>
   );
 }
